86dda9ab3b61887ff9f979591d9211f3024f1e9f
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-18 22:43:51 +0000

    updated password type in sign up

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const AuthController = require('../controllers/auth');
-
-//router.get('/', auth, AuthController.Index);
-router.post('/', AuthController.Login);
-
-module.exports = router;
--- backend/spec/jestVars.js
+++
@@ -1,2 +0,0 @@
-process.env.GEOCODER_PROVIDER = 'mapquest';
-process.env.GEOCODER_API = 'OO4tz4MA8TzdoKClaMGIPHnC6GZXDzqM';
